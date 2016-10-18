a194bba28c0051b55fa31a3933d8a151adb20999
Claude477 <esa.korkiakoski@gmail.com>
2016-10-18 18:58:16 +0300

    Source

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿
+
+using System;
+namespace ascii_translator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            string txt = textBox1.Text;
+            textBox1.Text = "";
+            for (int i = 0; i < txt.Length; i++)
+            {
+                char[] c = txt.ToString().ToCharArray();
+                textBox1.Text += Convert.ToInt32(c[i]).ToString()+" ";
+            }
+        }
+    }
+}
